058bc5fbb3f10660bc0e21eeac351f26e1428e7d
SidHarder <softwarenavigator@gmail.com>
2017-04-27 07:36:45 -0600

    working on fexex

--- src/core/update-fedex-shipments.js
+++ src/core/update-fedex-shipments.js
@@ -7,8 +7,7 @@ const PROTO_PATH = path.resolve('./node_modules/ap-protobuf/src/fedex/fedex.prot
 const fedex_proto = grpc.load(PROTO_PATH).fedex
 const client = new fedex_proto.UpdateFedexShipments('localhost:50052', grpc.credentials.createInsecure())
 
-module.exports = {
-  update: (callback) => client.update({ message: 'update' }, function(err, response) {
+module.exports.run = (callback) => client.update({ message: 'update' }, function(err, response) {
     callback(null, 'Fedex shipments have been updated.')
   })
 }
