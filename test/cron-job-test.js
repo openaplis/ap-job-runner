52b68eb5949cb48a025822fc95413192cf2e165a
SidHarder <softwarenavigator@gmail.com>
2017-06-06 09:51:27 -0600

    adjusting schedule

--- test/cron-job-test.js
+++ test/cron-job-test.js
@@ -1,6 +1,6 @@
 const assert = require('chai').assert
 const CronJob = require('cron').CronJob
 
-var updateFexShipmentsJob = new CronJob('0 */60 16-20 * * 1-5', function() {
+var updateFexShipmentsJob = new CronJob('0 */2 8-6 * * 1-5', function() {
   console.log('Cron job has run')
 }, null, true, null)
